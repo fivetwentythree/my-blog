92753d432a7a2f02f9e0742fa56e927d44b88c74
Lochana Perera <lochana-mbp@Lochanas-MacBook-Pro.local>
2025-01-31 21:45:58 +1100

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://fivetwentythree.github.io",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
